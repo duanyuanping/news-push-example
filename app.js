88706a7d6138cd4fd7fbb5ff01b5626802690261
duanyuanping <1798482104@qq.com>
2020-07-25 21:47:19 +0800

    feat: 测试

---
+++ app.js
@@ -0,0 +1,101 @@
+  
+const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+const session = require('koa-session');
+const bodyParser = require('koa-bodyparser');
+const webPush = require('web-push');
+const { resolveTxt } = require('dns');
+
+const port = '7001';
+const app = new Koa();
+const sessionConfig = {
+  key: 'news_push',
+  maxAge: 86400000,
+  autoCommit: true,
+  overwrite: true,
+  httpOnly: true,
+  signed: true,
+  rolling: false,
+  renew: false,
+  secure: false,
+  encode: (json) => JSON.stringify(json), // 自定义cookie编码函数
+  decode: (str) => JSON.parse(str),
+};
+
+// secret
+app.keys = ['news push demo'];
+
+// session
+app.use(session(sessionConfig, app));
+
+// 静态资源
+app.use(serve(__dirname + '/public'));
+
+// 请求内容解析
+app.use(bodyParser());
+
+// 路由
+const router = new Router();
+app.use(router.routes());
+// 业务服务器存放用户浏览器的订阅信息
+router.post('/subscript', async (ctx, next) => {
+	ctx.session.subscription = ctx.request.body;
+	ctx.body = {
+		retcode: 0,
+		msg: 'success'
+	};
+});
+
+/**
+ * @desc 向浏览器推送服务推送通知
+ * @desc 工具：web-push
+ */
+const options = {
+	// proxy: '', 
+};
+// VAPID
+const vapidKeys = {
+	publicKey: 'BEfibeblQcaa3wzDyGSPddFreazGqDZHDNa2jsfXRn-ni_SO2-jb_1NDJHE8tvRHEzvtujXXijZzGBQNbbMBvZI',
+	privateKey: 'YxDDrS1lnZZ0xAkp5YsCr2yOTIIQeXp9ayJukRCf8Aw'
+};
+webPush.setVapidDetails(
+	'mailto:1499685546@qq.com', // 开发者邮箱
+	vapidKeys.publicKey,
+	vapidKeys.privateKey
+);
+router.post('/push', async (ctx, next) => {
+	const { content, image } = ctx.request.body;
+	const { subscription } = ctx.session;
+
+	if (!subscription) return ctx.body = { retcode: 1, msg: '客户端尚未上传浏览器订阅信息'};
+	const pushDate = {
+		content,
+		image
+	};
+	const result = await webPush.sendNotification(subscription, JSON.stringify(pushDate), options).catch(err => {
+		if (err.statusCode === 404 || err.statusCode === 410) {
+			ctx.session.subscription = '';
+			return {
+				retcode: 1,
+				msg: '浏览器订阅信息无效'
+			};
+		} else {
+			console.log(err)
+			return {
+				retcode: 1,
+				msg: JSON.stringify(err)
+			};
+		}
+	});
+	
+	if (result.retcode) return ctx.body = result;
+	ctx.body = {
+		retcode: 0,
+		msg: 'success'
+	};
+});
+
+// 服务启动
+app.listen(port);
+console.log(`server listen at http://127.0.0.1:${port}`);
---
+++ public/index.js
@@ -0,0 +1,104 @@
+// 1、service worker注册
+// navigator.serviceWorker.register('./sw.js')
+
+// 2、授权通知
+function askPermission() {
+  return new Promise((res, rej) => {
+    Notification.requestPermission(permission => {
+      if (permission !== 'granted') {
+        console.log('通知授权失败');
+        rej();
+      } else {
+        console.log('通知授权成功')
+        res();
+      }
+    })
+  });
+}
+
+// 3、subscription
+async function subscript(registration, publickKey) {
+  const options = {
+    userVisibleOnly: true,
+    applicationServerKey: window.urlBase64ToUint8Array(publickKey)
+  };
+  const subscription = await registration.pushManager.subscribe(options);
+  return subscription;
+}
+
+// 4、push service
+
+async function min() {
+  /**
+   * install web-push
+   * web-push generate-vapid-keys
+   * Public Key:
+   * BEfibeblQcaa3wzDyGSPddFreazGqDZHDNa2jsfXRn-ni_SO2-jb_1NDJHE8tvRHEzvtujXXijZzGBQNbbMBvZI
+   * Private Key:
+   * YxDDrS1lnZZ0xAkp5YsCr2yOTIIQeXp9ayJukRCf8Aw
+   */
+  const publickKey = 'BEfibeblQcaa3wzDyGSPddFreazGqDZHDNa2jsfXRn-ni_SO2-jb_1NDJHE8tvRHEzvtujXXijZzGBQNbbMBvZI';
+  const [registration] = await Promise.all([
+    navigator.serviceWorker.register('./sw.js'),
+    askPermission()
+  ]);
+  const subscription = await subscript(registration, publickKey);
+  console.log(subscription)
+
+  const response = await fetch('/subscript', {
+    method: 'post',
+    body: JSON.stringify(subscription),
+    headers: {
+      'content-type': 'application/json'
+    },
+    credentials: 'same-origin'
+  }).then(res => res.json());
+  if (response.retcode === 0) {
+    console.log('订阅信息已存放到服务器');
+  } else {
+    console.log(response.msg)
+  }
+
+  // 本地通知和服务器消息推送
+  const noticeDom = document.getElementById('notice');
+  const pushDom = document.getElementById('push');
+  const contentDom = document.getElementById('inputContent');
+  const imageDom = document.getElementById('inputImage');
+  // 触发本地通知
+  noticeDom.onclick = () => {
+    const content = contentDom.value;
+    const image = imageDom.value;
+    const title = '网页消息推送demo';
+    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+    const options = {
+      body: content || '网页本地通知',
+      icon: image || `${window.location.href}1.png`,
+      actions: [{
+        action: 'github',
+        title: 'github'
+      }]
+    }
+    registration.showNotification(title, options);
+  }
+
+  // 触发推送服务通知
+  pushDom.onclick = async () => {
+    const content = contentDom.value;
+    const image = imageDom.value;
+    const response = await fetch('/push', {
+      method: 'post',
+      body: JSON.stringify({content, image}),
+      headers: {
+        'content-type': 'application/json'
+      },
+      credentials: 'same-origin'
+    }).then(res => res.json());
+    if (response.retcode === 0) {
+      console.log('服务器消息推送成功');
+    } else {
+      console.log(response.msg)
+    }
+  }
+}
+
+window.onload = min();
